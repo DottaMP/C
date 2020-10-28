7b9f1b268894b7135a5bb25859a269c3f0495718
DottaMP <dotta.may@gmail.com>
2020-10-27 22:42:31 -0300

    For

---
+++ Logica de Programacao/imprimirImparesFor.main.c
@@ -0,0 +1,17 @@
+/*Elabore um algoritmo que gere e escreva os números ímpares dos números entre 100 e 200.*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    
+    int i;
+		
+		for(i=100; i<=200; i++){
+			if(i%2!=0)
+			{
+				printf("%i,", i);
+			}
+		}
+	
+    return 0;
+}
---
+++ Logica de Programacao/tabuadaFor.main .c
@@ -0,0 +1,20 @@
+/*Faça um algoritmo que dado valor inteiro informado, calcule e exiba a tabuada do número de 1 a 10.*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    
+    int i, tabuada=0;
+    
+    printf("Informe qual tabuada você deseja calcular: ");
+    scanf("%i", &tabuada);
+		
+		for(i=1; i<=10; i++){
+		    
+		    printf("%ix%i = %i\n", i, tabuada, i * tabuada);
+			
+		}
+	
+    return 0;
+}
+
