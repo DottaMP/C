e2e492f484c5e3669fdfc6f5a21ab3608198a915
Mayara Dotta <67791520+DottaMP@users.noreply.github.com>
2020-10-27 22:50:53 -0300

    If / Else

--- Logica de Programacao/maiorMenorMeioIfEncadeado.main.c
+++ Logica de Programacao/maiorMenorMeioIfEncadeado.main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 
 /* Informe três números inteiros, utilizando comandos de seleção encadeados, defina 
