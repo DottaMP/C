6eb843a51ca3e27623a2da05c50ebea19f495752
Mayara Dotta <67791520+DottaMP@users.noreply.github.com>
2020-10-27 22:46:16 -0300

    If / Else

--- Logica de Programacao/calculoComissao.main.c
+++ Logica de Programacao/calculoComissao.main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 /*  Desenvolva um programa em C do problema a seguir: Utilizando comandos de seleção aninhados, 
 identifique e calcule a comissão de um vendedor baseado no valor de venda informado:
