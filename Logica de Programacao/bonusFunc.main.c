eaf98609db11113bcf94cb695e5d6b28c86802f3
Mayara Dotta <67791520+DottaMP@users.noreply.github.com>
2020-10-27 22:45:20 -0300

    If / Else

--- Logica de Programacao/bonusFunc.main.c
+++ Logica de Programacao/bonusFunc.main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 
 /* A empresa XPTO concedeu um bônus de 20% do valor do salário a todos os funcionários 
