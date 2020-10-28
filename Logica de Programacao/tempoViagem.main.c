1723eaf5eec811bf3177d2df4044233b124e9f74
Mayara Dotta <67791520+DottaMP@users.noreply.github.com>
2020-10-27 22:54:37 -0300

    Lógica Simples

--- Logica de Programacao/tempoViagem.main.c
+++ Logica de Programacao/tempoViagem.main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 
 int main()
@@ -24,7 +16,7 @@ int main()
         tempo = distancia / velocidade;
         consumoNecessario = distancia / consumo;
 		
-		printf("Tempo de Viagem: %.2lf\n", tempo);
+	printf("Tempo de Viagem: %.2lf\n", tempo);
         printf("Será necessário %.0f litros", consumoNecessario);
 
 	return 0;
