b9c1f9b4c37120b6130e9c6ef40fc07176c625c4
Mayara Dotta <67791520+DottaMP@users.noreply.github.com>
2020-10-27 22:50:02 -0300

    Lógica Simples

--- Logica de Programacao/converterKmMilhas.main.c
+++ Logica de Programacao/converterKmMilhas.main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 
 /*Faça um programa em C que dada uma distância em km converta e exiba em milhas, onde 1Milha = 1,60934km*/
