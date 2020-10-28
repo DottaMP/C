abb2e3942b413f094ee0710954ade944706432c7
Mayara Dotta <67791520+DottaMP@users.noreply.github.com>
2020-10-27 22:55:26 -0300

    If / Else If / Else

--- Logica de Programacao/velocidadePermitidaMulta.main.c
+++ Logica de Programacao/velocidadePermitidaMulta.main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 
 /* Leia a velocidade máxima permitida em uma avenida e a velocidade com que o motorista estava 
