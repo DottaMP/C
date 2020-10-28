dbe41bafb0ce3d4b15021e95722301e0de0c7cbf
Mayara Dotta <67791520+DottaMP@users.noreply.github.com>
2020-10-27 22:53:45 -0300

    If / Else

--- Logica de Programacao/parImpar.main.c
+++ Logica de Programacao/parImpar.main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 
 /*Faça um algoritmo que receba um número inteiro, identifique se este e par ou impar.*/
