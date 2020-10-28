cca6d0b3c3ef1d71ee8e9fa7d263f0328acdb487
Mayara Dotta <67791520+DottaMP@users.noreply.github.com>
2020-10-27 22:44:12 -0300

    If / Else If / Else

--- Logica de Programacao/anoBissextoQtoFalta.main.c
+++ Logica de Programacao/anoBissextoQtoFalta.main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 
 /* Faça um programa em C que verifique se o ano é bissexto ou não. Se ele não for 
