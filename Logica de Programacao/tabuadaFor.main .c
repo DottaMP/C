50b1c2fc6c1f440d023c4eea0fee4450af0e50c9
Mayara Dotta <67791520+DottaMP@users.noreply.github.com>
2020-11-14 17:37:24 -0300

    For

--- Logica de Programacao/tabuadaFor.main .c
+++ Logica de Programacao/tabuadaFor.main .c
@@ -1,16 +1,14 @@
 //#include <stdio.h>
 /*Faça um algoritmo que dado valor inteiro informado, calcule e exiba a tabuada do número de 1 a 10.*/
 
-int main(int argc, char *argv[]){
-    
+int main(int argc, char *argv[]){ 
     int i, tabuada=0;
-    
+ 
     printf("Informe qual tabuada você deseja calcular: ");
     scanf("%i", &tabuada);
 	for(i=1; i<=10; i++){    
 	    printf("%ix%i = %i\n", i, tabuada, i * tabuada);
 	}
-	
     return 0;
 }
 
